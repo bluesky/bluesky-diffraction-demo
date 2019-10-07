8696c8c99f178ee9da03129e32cb3c5403faa877
ke-zhang-rd <kezhang@bnl.gov>
2019-10-07 10:04:08 -0400

    ENH: Get Sample, Plan and History List

--- sample_entry.py
+++ sample_entry.py
@@ -1,21 +1,25 @@
 import amostra.mongo_client
 from ipysheet import cell, sheet
-from ipywidgets import link
+from ipywidgets import link, Layout
+import ipywidgets as widgets
 from traitlets import HasTraits, Unicode, Float
+import time
 import uuid
 
 
 COLUMN_RATIO = [1, 1, 1]
-
+SAMPLE_LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 client = amostra.mongo_client.Client(
         f'mongodb://localhost:27017/test_amostra_ipysheet-{uuid.uuid4()!s}')
 
-samples_sheet = sheet(rows=5, columns=3,
+
+samples_sheet = sheet(rows=6, columns=3,
                       column_width=COLUMN_RATIO,
                       column_headers=['Sample Name', 'Composition', 'UUID'])
+
 for row in range(samples_sheet.rows):
     sample = client.samples.new(name=' ', composition=' ')
-    link((cell(row, 0, value=' '), 'value'), (sample, 'name'))
+    link((cell(row, 0, value=SAMPLE_LIST[row]), 'value'), (sample, 'name'))
     link((cell(row, 1, value=' '), 'value'), (sample, 'composition'))
     cell(row, 2, value=' ').value = sample.uuid
 
@@ -37,18 +41,17 @@ def search(change):
     '''
     n = change['new']
     c = change['owner']
-    update_cell = cell(c.row_start, c.column_end+2)
+    update_cell = metadata_sheet[c.row_start, c.column_end+2]
     for s in client.samples.find({}):
         if n == s.name:
             update_cell.value = s.uuid
             check_list[c.row_start][0] = True
-            cell(c.row_start, c.column_start, c.value,
-                 background_color='white')
+            metadata_sheet[c.row_start, c.column_start].style = {'backgroundColor': 'white'}
             break
     else:
         update_cell.value = ''
         check_list[c.row_start][0] = False
-        cell(c.row_start, c.column_start, c.value, background_color='grey')
+        metadata_sheet[c.row_start, c.column_start].style = {'backgroundColor': 'grey'}
 
 
 def check(change):
@@ -56,20 +59,18 @@ def check(change):
     c = change['owner']
     if isinstance(s, int) and s > 0:
         check_list[c.row_start][1] = True
-        cell(c.row_start, c.column_start, c.value, background_color='white')
+        metadata_sheet[c.row_start, c.column_start].style = {'backgroundColor': 'white'}
     else:
         check_list[c.row_start][1] = False
-        cell(c.row_start, c.column_start, c.value, background_color='grey')
+        metadata_sheet[c.row_start, c.column_start].style = {'backgroundColor': 'grey'}
 
 
-metadata_sheet = sheet(rows=5, columns=3, column_width=COLUMN_RATIO,
+metadata_sheet = sheet(rows=6, columns=3, column_width=COLUMN_RATIO,
                        column_headers=['Sample Name', 'ScanTime', 'UUID'])
-work_list = [None, None, None, None, None]
-check_list = [[False, False],
-              [False, False],
-              [False, False],
-              [False, False],
-              [False, False]]
+
+work_list = [None for i in range(metadata_sheet.rows)]
+check_list = [[False, False] for i in range(metadata_sheet.rows)]
+
 for row in range(metadata_sheet.rows):
     work_list[row] = WorkQueueItem(name=' ', scantime=0)
     # sample cell
@@ -84,30 +85,67 @@ for row in range(metadata_sheet.rows):
     uuid_cell = cell(row, 2, value=' ')
     link((uuid_cell, 'value'), (work_list[row], 'uuid'))
 
+log_sheet = sheet(rows=15, columns=3, column_width=COLUMN_RATIO,
+                       column_headers=['Sample Name', 'ScanTime', 'Upload'])
+log_sheet.layout.height = '200px'
 
-from bluesky.plans import count
+
+def on_button_clicked(b):
+    for i, e in enumerate(check_list):
+        if not any(e):
+            first_empty_row = i
+            break
+
+    for i in range(2):
+        metadata_sheet[first_empty_row, i].value = log_sheet[row_of_button[b], i].value
+
+row_of_button = dict()
+for row in range(log_sheet.rows):
+    cell(row, 0, value=' ')
+    cell(row, 1, value=None)
+
+    button = widgets.Button(description="Push back")
+    button.on_click(on_button_clicked)
+    row_of_button[button] = row
+    cell(row, 2, button)
+
+
+from bluesky.plans import count, scan
 from bluesky.plan_stubs import mv
-from startup import det
-import time
+from startup import det, motor
+
+# Init current_log_row which is the location of current available line
+current_log_row = 0
 
 
 def plan_factory(work_list):
     for r, w in enumerate(work_list):
-        time.sleep(3)
+        # If all field is legal, run plan
         if all(check_list[r]):
             print(f'Lock row {1+r} and \N{Rocket}')
+            time.sleep(1)
             for c in range(2):
-                tmp = cell(r,c, metadata_sheet[r,c].value, read_only=True)
-                #tmp = cell(r,c, '\N{Rocket}', read_only=True)
+                tmp = metadata_sheet[r,c]
+                tmp.style = {'backgroundColor': 'lightblue'}
+                tmp.read_only = True
+                tmp.send_state()
+                global current_log_row
+                current_log_cell = log_sheet[current_log_row,c]
+                current_log_cell = cell(current_log_row, c, tmp.value)
 
+            current_log_row = current_log_row + 1
             yield from mv(det.exp, w.scantime)
-            yield from count([det])
+            uid = yield from count([det])
         else:
             print(f'Skip row {1+r}')
     # Reset back to read_only = False
+
     for r in range(len(work_list)):
         for c in range(2):
-            tmp = cell(r,c, metadata_sheet[r,c].value, read_only=False)
+            tmp = metadata_sheet[r,c]
+            tmp.read_only=False
+            if tmp.style['backgroundColor'] == 'lightblue':
+                tmp.style = {'backgroundColor': 'white'}
 
 
 plans = plan_factory(work_list)
